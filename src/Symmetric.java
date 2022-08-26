34e4ffb5eb3d3ce855d71085a00d8aa2e5b01429
chebihiF <chebihi.f@ucd.ac.ma>
2022-08-26 11:39:52 +0100

    final version symmetric

--- src/Symmetric.java
+++ src/Symmetric.java
@@ -11,6 +11,41 @@ public class Symmetric {
         try {
             SecretKey key = createAESKey();
             System.out.println("The symmetric key : "+ hexString(key.getEncoded()));
+            byte[] initializationVector = createInitializationVector();
+            String plainText = "this is the message i want to Encrypt";
+            try {
+                System.out.println("Message : "+plainText);
+                System.out.println("Encrypting in progress ...");
+                byte[] cipherText = do_AESEncryption(plainText,key,initializationVector);
+                System.out.println("Result : "+hexString(cipherText));
+
+                System.out.println("Decrypting in progress ...");
+                byte[] plainTextBytes = do_AESDecryption(cipherText,key,initializationVector);
+                plainText = new String(plainTextBytes);
+                System.out.println("Message : "+ plainText);
+
+                initializationVector = createInitializationVector();
+                cipherText = do_AESEncryption(plainText,key,initializationVector);
+                System.out.println("Result : "+hexString(cipherText));
+                System.out.println("Decrypting in progress ...");
+                plainTextBytes = do_AESDecryption(cipherText,key,initializationVector);
+                plainText = new String(plainTextBytes);
+                System.out.println("Message : "+ plainText);
+
+
+
+            } catch (NoSuchPaddingException e) {
+                throw new RuntimeException(e);
+            } catch (InvalidAlgorithmParameterException e) {
+                throw new RuntimeException(e);
+            } catch (InvalidKeyException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalBlockSizeException e) {
+                throw new RuntimeException(e);
+            } catch (BadPaddingException e) {
+                throw new RuntimeException(e);
+            }
+
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
@@ -24,17 +59,17 @@ public class Symmetric {
     }
 
     // this methode take a plain text and encrypt it to CipherText
-    private static byte[] do_AESEncryption(String text, SecretKey key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    private static byte[] do_AESEncryption(String text, SecretKey key, byte[] initializationVector) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(createInitializationVector());
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(initializationVector);
         cipher.init(Cipher.ENCRYPT_MODE, key, ivParameterSpec);
         return cipher.doFinal(text.getBytes());
     }
 
     // this methode take CipherText and convert to plain text
-    private static byte[] do_AESDecryption(byte[] cipherText, SecretKey key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    private static byte[] do_AESDecryption(byte[] cipherText, SecretKey key, byte[] initializationVector) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(createInitializationVector());
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(initializationVector);
         cipher.init(Cipher.DECRYPT_MODE, key, ivParameterSpec);
         return cipher.doFinal(cipherText);
     }
